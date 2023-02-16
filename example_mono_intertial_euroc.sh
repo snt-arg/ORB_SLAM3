a1f30d322d1b1b8bcf240525750664174f9b3a9d
Meisam-Kabiri <msm.kabiri91@gmail.com>
2023-02-16 10:57:33 +0100

    Add examples for euroc

---
+++ example_mono_intertial_euroc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pathDatasetEuroc='Datasets/EuRoC' #Example, it is necesary to change it by the dataset path
+
+#------------------------------------
+# Monocular Examples
+echo "Launching MH01 with Monocular-Inertial sensor"
+./Examples/Monocular-Inertial/mono_inertial_euroc ./Vocabulary/ORBvoc.txt ./Examples/Monocular-Inertial/EuRoC.yaml "$pathDatasetEuroc"/MH01 ./Examples/Monocular-Inertial/EuRoC_TimeStamps/MH01.txt dataset-MH01_monoi
