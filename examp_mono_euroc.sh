26d4b86bc509c009b8d23f5ada0586a0b902c10e
Meisam-Kabiri <msm.kabiri91@gmail.com>
2023-02-23 15:35:08 +0100

    modify the examp_mono_euroc

--- examp_mono_euroc.sh
+++ examp_mono_euroc.sh
@@ -4,4 +4,4 @@ pathDatasetEuroc='Datasets/EuRoC' #Example, it is necesary to change it by the d
 #------------------------------------
 # Monocular Examples
 echo "Launching MH01 with Monocular sensor"
-./Examples/Monocular/mono_euroc ./Vocabulary/ORBvoc.txt ./Examples/Monocular/EuRoC.yaml "$pathDatasetEuroc"/MH01 ./Examples/Monocular/EuRoC_TimeStamps/MH01.txt dataset-MH01_mono
+./Examples/Monocular/mono_euroc ./Vocabulary/ORBvoc.txt ./Examples/Monocular/EuRoC.yaml "$pathDatasetEuroc"/V101 ./Examples/Monocular/EuRoC_TimeStamps/V101.txt V101_monoeuroc
